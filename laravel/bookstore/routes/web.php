659abe8159b33a617ebcd06d7e04f52acdb24d8a
Ejsanchez <erik.sanchez.maldonado@gmail.com>
2022-04-18 11:30:02 -0500

    Agrego comentarios de apoyo para comprensión de routing

--- laravel/bookstore/routes/web.php
+++ laravel/bookstore/routes/web.php
@@ -13,7 +13,29 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+/*
+La clase Route de laravel es la encargada de definir las rutas que se usarán en la apliación.
+Entre otros métodos, cuenta con un método estático para cada posible acción de HTTP:
+post, get, put, delete, patch, etc.
+*/
+
+/*
+La ruta más básica de PHP recibe:
+    + Un string con el path (relativo al dominio de la aplicación) con el que se hará match
+    + Una función anónima (closure) que funcionará como controlador a ejecutarse al cargar la ruta
+      en el navegador.
+La función controladora debe devolver una respuesta que se mandará en el navegador (típicamente HTML)
+*/
 Route::get('/', function () {
+    /*
+    "view" es un método predefinido (helper) de Laravel que devuelve el contenido de una vista que debe
+    estar almacenada en el directorio "resources/views".
+    
+    El patrón de nombre de las vistas es "{viewName}.blade.php, el string pasado como parámetro a la 
+    función "view" corresponde a {viewName}.
+    
+    La extensión ".blade" se debe a que el "template engine" usado por laravel se llama "Blade".
+    */
     return view('welcome');
 });
 
@@ -27,6 +49,10 @@ Route::get('/', function () {
 //     return view("aboutUs");
 // });
 
-//Método devuelve una vista directamente sin pasar por el controller
+/*
+Cuando la función controladora no ejecutará ninguna lógica más que devolver una vista estática,
+se puede usar como "atajo" el método estático "view" que funcionará como un get que hace un
+return de una vista.
+*/
 Route::view("/about-us", "aboutUs");
 
