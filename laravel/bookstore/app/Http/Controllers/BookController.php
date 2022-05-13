6323dd44d6aa2acfe7b6d22107ff8e10a72794db
Ejsanchez <erik.sanchez.maldonado@gmail.com>
2022-05-13 16:08:16 -0500

    Se termina la lógica para guardar un libro usando el formulario, incluyendo la creacińo de registros en la tabla "books" y sus asociaciones con autores y categorías, el almacenamiento de la portada y manejo de errores.

--- laravel/bookstore/app/Http/Controllers/BookController.php
+++ laravel/bookstore/app/Http/Controllers/BookController.php
@@ -9,6 +9,13 @@ use App\Models\Author;
 use App\Models\Category;
 use Illuminate\Http\Request;
 
+//Excepciones de BD de laravel
+use Illuminate\Database\QueryException;
+
+//Clase para el manejo del storage (archivos) de laravel
+use Illuminate\Support\Facades\Storage;
+
+
 class BookController extends Controller
 {
     /**
@@ -53,27 +60,78 @@ class BookController extends Controller
      */
     public function store(Request $request){
 
-        //Guardar el libro
-        $newBook = Book::create([
-            "isbn" => $request->input("isbn"),
-            "title" => $request->input("title"),
-            "summary" => $request->input("summary"),
-            "year" => $request->input("year"),
-            "edition" => $request->input("edition"),
-            "price" => $request->input("price"),
-            "cover" => "",//$request->input("cover"),
-            "publisher_id" => $request->input("publisher"),
-            "language_id" => $request->input("language")
-        ]);
+        //dd($request->all());
 
-        // //Autores
-        // foreach($request->input("authors") as $authorId){
+        //Procesamiento del archivo de portada
+        //https://laravel.com/docs/9.x/filesystem#file-uploads
 
-        //      $book->authors() 
+        //Generar un nombre único del archivo
+        $fileName = $request->file("cover")->hashName();
 
-        // }
+        //Guardar el archivo en el storage
+        $coverPath = $request->file("cover")->storeAs(
+            "books_covers", //Directorio
+            $fileName, //Nombre del archivo
+            "public" //Disco donde se guardará (configurado en clonfig/filesystems.php)
+        );
 
-        // //Categorías
+        try{
+
+            //Guardar el libro
+            $newBook = Book::create([
+                "isbn" => $request->input("isbn"),
+                "title" => $request->input("title"),
+                "summary" => $request->input("summary"),
+                "year" => $request->input("year"),
+                "edition" => $request->input("edition"),
+                "price" => $request->input("price"),
+                "cover" => $fileName,
+                "publisher_id" => $request->input("publisher"),
+                "language_id" => $request->input("language")
+            ]);
+
+            //Autores
+            //https://laravel.com/docs/9.x/eloquent-relationships#updating-many-to-many-relationships
+            foreach($request->input("authors") as $authorId){
+
+                $newBook->authors()->attach($authorId);
+
+            }
+
+            //Categorías
+            // Igual que con autores
+            foreach($request->input("category") as $categoryId){
+
+                $newBook->categories()->attach($categoryId);
+
+            }
+        }
+        catch(QueryException $ex){
+
+            //Borrar el archivo (https://laravel.com/docs/9.x/filesystem#deleting-files) 
+            Storage::disk('public')->delete($coverPath);
+
+            //Algo salió mal, redirigir al catálogo de libros con mensaje de error
+            //https://laravel.com/docs/9.x/responses#redirecting-named-routes
+            return redirect()
+                    ->route("books.index")
+                    //https://laravel.com/docs/9.x/responses#redirecting-with-flashed-session-data
+                    ->with("message", [
+                        "type" => "error",
+                        "text" => "Ha ocurrido un error al guardar el libro"
+                    ]);
+
+        }
+
+       //Todo salió bien, redirigir al catálogo de libros
+       //https://laravel.com/docs/9.x/responses#redirecting-named-routes
+       return redirect()
+                ->route("books.index")
+                //https://laravel.com/docs/9.x/responses#redirecting-with-flashed-session-data
+                ->with("message", [
+                    "type" => "success",
+                    "text" => "El libro se guardó exitosamente"
+                ]);
 
     }
 
